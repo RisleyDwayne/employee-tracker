f2b6dd9515b8bcb04c75333def49f081ea5e58b5
Dwayne Risley <risleydwayne@gmail.com>
2020-09-19 00:00:50 -0400

    main app code

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
-const mysql = require("mysql");
+const DAL = require("./lib/DAL");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
-
 const connection = require("./lib/dbConnection");
+const createFuncs = require("./lib/create");
+const readFuncs = require("./lib/read");
+const updateFuncs = require("./lib/update");
+const deleteFuncs = require("./lib/delete");
 
 
 const prompt = (prompts) => {
     return inquirer.prompt(prompts);
+}
+
+const actionFuncs = {
+    'View All Employees': readFuncs.readEmployees,
+    'View All Employees by Department': readFuncs.readEmployeesByDept,
+    'View All Employees by Manager': readFuncs.readEmployeesByMgr,
+    'Add Employee': createFuncs.createEmployee,
+    'Remove Employee': deleteFuncs.deleteEmployee,
+    'Update Employee Role': updateFuncs.updateEmpRole,
+    'Update Employee Manager': updateFuncs.updateEmpMgr,
+    'View All Roles': readFuncs.readRoles,
+    'Add Role': createFuncs.createRole,
+    'Remove Role': deleteFuncs.deleteRole,
+    'View All Departments': readFuncs.readDepartments,
+    'View Budget by Department': readFuncs.readBudgetByDept,
+    'Add Department': createFuncs.createDepartment,
+    'Remove Department': deleteFuncs.deleteDepartment,
+    'Exit Application': exit
 }
 
 const userActions = [
@@ -36,6 +57,29 @@ const userActions = [
 
 ];
 
-function beginApp(){
-    
+function exit() {
+    console.log("Application closing...");
+    connection.end();
+    process.exit();
+};
+
+async function init(){
+
+    try {
+        console.log("\n------------------------\n");
+        const choice = await prompt(userActions);
+        console.log("\n------------------------\n");
+        await actionFuncs[choice.action]();
+        init();
+    }catch (err) {
+        console.log(err);
+    }
+
 }
+
+function start(){
+    console.log("Welcome to the employee tracker app! \n");
+    init();
+}
+
+start();
